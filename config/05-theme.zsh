d1b84b11179878e4ace574c8343a243847db673e
Abid Omar <contact@omarabid.com>
2020-09-28 01:41:42 +0100

    Theme customization

---
+++ config/05-theme.zsh
@@ -0,0 +1,14 @@
+# Theme style 
+export ALIEN_THEME="gruvbox"
+
+# Left Prompt
+export ALIEN_SECTIONS_LEFT=(
+  user
+  path
+  vcs_branch:async
+  vcs_dirty:async
+  newline
+  ssh
+  venv
+  prompt
+)
