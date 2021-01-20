aaf3b1f2f8a788cc7d04f9df5277d1ed48ee3537
Abid Omar <contact@omarabid.com>
2021-01-20 23:06:06 +0100

    add autocompletion config

--- config/02-autocompletion.zsh
+++ config/02-autocompletion.zsh
@@ -1,4 +1,8 @@
 autoload -U compinit
 zstyle ':completion:*' menu select
+zstyle ":completion:*:git-checkout:*" sort false
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 zmodload zsh/complist
 compinit
